82d7774a1aa8c6f467d47f1133e1cbc23f86ded3
ftrbnd <giosalas25@gmail.com>
2023-05-11 12:50:58 -0700

    Create nav

---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../styles/Nav.module.css';
+
+const Nav = () => (
+  <div className={styles.Nav}>
+    Nav Component
+  </div>
+);
+
+export default Nav;
--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
+import Shop from "./Shop";
+import Nav from "./Nav";
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+        <Routes>
+            <Nav />
+            <Route path="/" element={<App />} />
+            <Route path="/shop" element={<Shop />} />
+        </Routes>
     </BrowserRouter>
   );
 };
